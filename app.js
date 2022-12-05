92cb6043c3ce5e5d03296df6c33337e5801692f5
AldiAbdillah <you@example.com>
2022-12-05 15:35:32 +0700

    mencoba express js

--- app.js
+++ app.js
@@ -1,49 +1,80 @@
 
-const http = require('http');
-const fs = require('fs')
+// const http = require('http');
+// const fs = require('fs')
+// const port = 3000
+
+// const renderHtml = (path, res) => {
+//     fs.readFile(path, (err, data) => {
+//         if(err) {
+//             res.writeHead(404)
+//             res.write('Error file tidak ada')
+//         } else {
+//             res.write(data)
+//         }
+//         res.end()
+//     })  
+// }
+
+// http.createServer((req, res) => {
+//     res.writeHead(200, {
+//         'Context-type' : 'text/html'
+//     })
+
+//     const url = req.url;
+
+//     switch (url) {
+//         case '/about':
+//             renderHtml('./about.html', res)
+//             break;
+//         case '/kontak':
+//             renderHtml('./contact.html', res)
+//         default:
+//             renderHtml('./index.html', res)
+//             break;
+//     }
+
+
+//     // if(url === '/about') {
+//     //     renderHtml('./about.html', res)
+//     // } else if (url === '/contact') {
+//     //     renderHtml('./contact.html', res)
+//     // } else {
+//     //     renderHtml('./index.html', res)
+//     // }
+
+
+
+// }) .listen(port, () => {
+//     console.log(`Server menyala di port ${port}`)
+// })
+
+// yang diatas adalah kode node js
+
+const express = require('express')
+const app = express()
 const port = 3000
 
-const renderHtml = (path, res) => {
-    fs.readFile(path, (err, data) => {
-        if(err) {
-            res.writeHead(404)
-            res.write('Error file tidak ada')
-        } else {
-            res.write(data)
-        }
-        res.end()
-    })  
-}
+app.get('/', (req, res) => {
+  res.sendFile('./index.html', {root: __dirname})
+})
 
-http.createServer((req, res) => {
-    res.writeHead(200, {
-        'Context-type' : 'text/html'
-    })
+app.get('/tentangkami', (req, res) => {
+  res.sendFile('./about.html', {root: __dirname})
+})
 
-    const url = req.url;
+app.get('/kontak', (req, res) => {
+  res.sendFile('./contact.html', {root: __dirname})
+})
 
-    switch (url) {
-        case '/about':
-            renderHtml('./about.html', res)
-            break;
-        case '/kontak_saya':
-            renderHtml('./contact.html', res)
-        default:
-            renderHtml('./index.html', res)
-            break;
-    }
+app.get('/product/:id', (req, res) => {
+    res.send(`Product id : ${req.params.id} <br> Category id : ${req.query.cat}`)
+})
 
+app.use('/', (req, res) => {
+    res.status('404')
+    res.sendFile('./404.html', {root: __dirname})
+})
 
-    // if(url === '/about') {
-    //     renderHtml('./about.html', res)
-    // } else if (url === '/contact') {
-    //     renderHtml('./contact.html', res)
-    // } else {
-    //     renderHtml('./index.html', res)
-    // }
-
-
-
-}) .listen(port, () => {
-    console.log(`Server menyala di port ${port}`)
+app.listen(port, () => {
+  console.log(`server menyala di port ${port}`)
 })
