c83b00f5f19eb1ae2ce2a59ac892a40ef8b9fba9
AldiAbdillah <you@example.com>
2022-12-05 18:10:36 +0700

    15 express view engine

--- app.js
+++ app.js
@@ -1,69 +1,46 @@
 
-// const http = require('http');
-// const fs = require('fs')
-// const port = 3000
-
-// const renderHtml = (path, res) => {
-//     fs.readFile(path, (err, data) => {
-//         if(err) {
-//             res.writeHead(404)
-//             res.write('Error file tidak ada')
-//         } else {
-//             res.write(data)
-//         }
-//         res.end()
-//     })  
-// }
-
-// http.createServer((req, res) => {
-//     res.writeHead(200, {
-//         'Context-type' : 'text/html'
-//     })
-
-//     const url = req.url;
-
-//     switch (url) {
-//         case '/about':
-//             renderHtml('./about.html', res)
-//             break;
-//         case '/kontak':
-//             renderHtml('./contact.html', res)
-//         default:
-//             renderHtml('./index.html', res)
-//             break;
-//     }
-
-
-//     // if(url === '/about') {
-//     //     renderHtml('./about.html', res)
-//     // } else if (url === '/contact') {
-//     //     renderHtml('./contact.html', res)
-//     // } else {
-//     //     renderHtml('./index.html', res)
-//     // }
-
-
-
-// }) .listen(port, () => {
-//     console.log(`Server menyala di port ${port}`)
-// })
-
-// yang diatas adalah kode node js
-
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
 app.get('/', (req, res) => {
-  res.sendFile('./index.html', {root: __dirname})
+  const mahasiswa = [
+    {
+      nama: 'Asep S',
+      email: 'asep@gmail.com'
+    },
+    {
+      nama: 'Agus S',
+      email: 'Agus@gmail.com'
+    },
+    {
+      nama: 'Adang S',
+      email: 'adang@gmail.com'
+    }
+  ]
+  res.render('index', {
+    layout: 'layouts/main-layout',
+    nama: 'Aldi Abdillah Basalamah',
+    title: 'Halaman Home 😁',
+    mahasiswa,
+    })
 })
 
-app.get('/tentangkami', (req, res) => {
-  res.sendFile('./about.html', {root: __dirname})
+app.get('/about', (req, res) => {
+  res.render('about', {
+    layout: 'layouts/main-layout', title: 'Halaman About'
+  })
 })
 
-app.get('/kontak', (req, res) => {
-  res.sendFile('./contact.html', {root: __dirname})
+app.get('/contact', (req, res) => {
+  res.render('contact', {
+    layout: 'layouts/main-layout',
+    title: 'Halaman Contact'
+  })
 })
 
 app.get('/product/:id', (req, res) => {
