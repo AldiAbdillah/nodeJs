39583516f6da1f6eb9f85c5d39fe6631f4e4a302
AldiAbdillah <you@example.com>
2022-12-05 19:19:18 +0700

    middleware

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs');
+
+// thirdparty middleware
 app.use(expressLayouts);
+app.use(morgan('dev'));
+
+// built-in middleware
+app.use(express.static('public'))
+
+// application level middleware
+app.use((req,res,next) => {
+  console.log('Time:', Date.now())
+  next();
+})
 
 app.get('/', (req, res) => {
   const mahasiswa = [
@@ -47,9 +60,9 @@ app.get('/product/:id', (req, res) => {
     res.send(`Product id : ${req.params.id} <br> Category id : ${req.query.cat}`)
 })
 
-app.use('/', (req, res) => {
-    res.status('404')
-    res.sendFile('./404.html', {root: __dirname})
+app.use((req, res) => {
+  res.status(404)
+  res.send('<h1>404</h1>')
 })
 
 app.listen(port, () => {
